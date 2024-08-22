81cf55bcda9d0dfd95d58a25f13bb8fa80ee3b1d
origin1508 <origin1508@gmail.com>
2024-08-22 20:54:11 +0900

    💄 Style: 가게 정보 스타일링 (#43)

--- src/app/posts/[postId]/_components/postContent/StoreInfo.tsx
+++ src/app/posts/[postId]/_components/postContent/StoreInfo.tsx
@@ -1,4 +1,4 @@
-import { Chip } from '@/components/atoms';
+import { Chip, Point } from '@/components/atoms';
 import { RatingStarSVG } from '@/components/svg/RatingstarSVG';
 
 interface StoreInfoProps {
@@ -10,25 +10,33 @@ interface StoreInfoProps {
 const StoreInfo = ({ name, starRatings, menuTag }: StoreInfoProps) => {
   return (
     <div>
-      <div className='grid grid-cols-2 divide-x divide-gray-100'>
-        <div className='flex flex-col items-center justify-center gap-2 p-[10px]'>
-          <div className='text-center text-gray-600 subTitle-18'>가게 이름</div>
-          <div className='text-gray-700 body-16'>{name}</div>
-        </div>
-        <div className='flex flex-col items-center justify-center gap-2 p-[10px]'>
-          <div className='text-center text-gray-600 subTitle-18'>별점</div>
-          <div className='flex items-center gap-1 text-primary-400 subTitle-20'>
-            <span>
-              <RatingStarSVG isFill='fill' />
-            </span>
-            <span>{starRatings}</span>
+      <div className='flex h-20 items-center'>
+        <div className='grid w-full grid-cols-2 divide-x divide-gray-100'>
+          <div className='flex items-center justify-center gap-2'>
+            <div className='text-gray-700 subTitle-20'>{name}</div>
+            <div>
+              <Point text={name} size='small' />
+            </div>
+          </div>
+          <div className='flex flex-col items-center justify-center gap-2'>
+            <div className='flex items-center gap-1 text-primary-400 subTitle-20'>
+              <span>
+                <RatingStarSVG />
+              </span>
+              <span>{starRatings}</span>
+            </div>
           </div>
         </div>
       </div>
-      <div className='flex items-center justify-center gap-2 py-[18px]'>
-        {menuTag.map((menu) => (
-          <Chip key={menu} text={menu} />
-        ))}
+      <div>
+        <div className='mb-2'>
+          <span className='text-gray-600 body-16'>추천 메뉴</span>
+        </div>
+        <div className='flex h-24 flex-wrap items-start gap-2'>
+          {menuTag.map((menu) => (
+            <Chip key={menu} text={menu} />
+          ))}
+        </div>
       </div>
     </div>
   );
