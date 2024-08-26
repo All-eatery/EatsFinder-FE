b385e203c49a5312de6b1ad16fbaa10f5199f278
origin1508 <origin1508@gmail.com>
2024-08-26 10:45:03 +0900

    ✨ Feat: 댓글 삭제, 댓글 작성 api (#43)

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -1,9 +1,38 @@
 import { KOTLIN_SERVER } from '@/constants/baseUrl';
 import { CommentType } from '@/types/comment';
 
-export const getComments = async (postId: string): Promise<CommentType[]> => {
+export const getComments = async (postId: number): Promise<CommentType[]> => {
   const res = await fetch(`${KOTLIN_SERVER}/posts/${postId}/comments`, {
     method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data = await res.json();
+
+  return data;
+};
+
+export const createComment = async (postId: number, content: string) => {
+  const body = JSON.stringify({ content });
+
+  const res = await fetch(`${KOTLIN_SERVER}/posts/${postId}/comments`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body,
+  });
+
+  const data = await res.json();
+
+  return data;
+};
+
+export const deleteComment = async (commentId: number) => {
+  const res = await fetch(`${KOTLIN_SERVER}/comments/${commentId}`, {
+    method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
     },
