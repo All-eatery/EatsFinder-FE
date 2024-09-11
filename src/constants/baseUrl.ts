b0e75f4d910bbca2bceaae8f931dc840a42131ee
origin1508 <origin1508@gmail.com>
2024-09-11 22:40:06 +0900

    ✨ Feat: KAKAO_REST_API_KEY 추가 (#53)

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,3 +1,4 @@
 export const NEST_SERVER = process.env.NEXT_PUBLIC_NEST_SERVER;
 export const KOTLIN_SERVER = process.env.NEXT_PUBLIC_KOTLIN_SERVER;
 export const KAKAO_API_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
+export const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
