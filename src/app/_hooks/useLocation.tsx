b2d80c5bb7224aab2279f789a51d0998be9cd710
origin1508 <origin1508@gmail.com>
2024-09-12 22:45:58 +0900

    ✨ Feat: geolocation을 이용한 지역검색 hook 작성 (#53)

---
+++ src/app/_hooks/useLocation.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import { getRegionCode } from '@/api/place';
+
+export const useLocation = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isGeolocationAvailable, setIsGeolocationAvailable] = useState(false);
+  const [region, setRegion] = useState<string>('서울특별시 강남구');
+
+  const getGeolocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        setIsLoading(true);
+        try {
+          const data = await getRegionCode(position);
+          const regionName = `${data.documents[0].region_1depth_name} ${data.documents[0].region_2depth_name}  ${data.documents[0].region_3depth_name}`;
+          setRegion(regionName);
+          setIsGeolocationAvailable(true);
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setIsLoading(false);
+        }
+      },
+      (err) => {
+        switch (err.code) {
+          case 1:
+            console.error(err.message);
+            break;
+          case 2:
+            console.error(err.message);
+            break;
+          case 3:
+            console.error(err.message);
+            break;
+          default:
+            console.error('unknown error!');
+            break;
+        }
+        setIsGeolocationAvailable(false);
+        setIsLoading(false);
+      },
+    );
+  };
+
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      getGeolocation();
+    }
+  }, []);
+
+  return {
+    region,
+    isLoading,
+    isGeolocationAvailable,
+    updateLocation: getGeolocation,
+  };
+};
