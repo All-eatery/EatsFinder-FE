9e649ab4adf6cfef27a6b9eb71d0821713e13544
origin1508 <origin1508@gmail.com>
2024-09-16 21:51:36 +0900

    ✨ Feat: useToggleHandler의 초기값 설정

--- src/hooks/useToggleHandler.ts
+++ src/hooks/useToggleHandler.ts
@@ -1,8 +1,8 @@
 'use client';
 import { useCallback, useState } from 'react';
 
-export const useToggleHandler = () => {
-  const [value, setValue] = useState<boolean>(false);
+export const useToggleHandler = (init = false) => {
+  const [value, setValue] = useState<boolean>(init);
   const handleValue = useCallback(() => {
     setValue((prev) => !prev);
   }, []);
