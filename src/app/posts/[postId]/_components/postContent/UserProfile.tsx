0c48bccb0e2e47c82649e62e0a819dcef888be53
origin1508 <origin1508@gmail.com>
2024-09-17 01:29:43 +0900

    ✨ Feat: UserProfile 스타일 수정, 게시일 추가, 좋아요 버튼, 공유 버튼 제거 (#43)

--- src/app/posts/[postId]/_components/postContent/UserProfile.tsx
+++ src/app/posts/[postId]/_components/postContent/UserProfile.tsx
@@ -1,26 +1,30 @@
-import { ProfileImage, Checkbox } from '@/components/atoms';
-import { ShareSVG } from '@/components/svg/ShareSVG';
+import { ProfileImage } from '@/components/atoms';
 import { MoreSVG } from '@/components/svg/MoreSVG';
+import timeDifference from '@/utils/timeDifference';
 
 interface UserProfileProps {
   nickname: string;
   profileImage: string | null;
+  createdAt: string;
 }
 
-const UserProfile = ({ nickname, profileImage }: UserProfileProps) => {
+const UserProfile = ({
+  nickname,
+  profileImage,
+  createdAt,
+}: UserProfileProps) => {
   return (
     <div className='flex justify-between'>
       <div className='flex items-center gap-3'>
-        <ProfileImage size={70} src={profileImage} />
-        <span>{nickname}</span>
+        <ProfileImage size={50} src={profileImage} />
+        <span className='text-gray-600 subTitle-18'>{nickname}</span>
+        <span className='text-gray-300 body-16'>
+          {timeDifference(createdAt)}
+        </span>
       </div>
       <div className='flex items-center justify-center gap-4'>
         <button className='[&>svg]:h-9 [&>svg]:w-9' aria-label='more'>
           <MoreSVG />
-        </button>
-        <Checkbox variant='fav' className='[&_svg]:h-9 [&_svg]:w-9' />
-        <button className='[&>svg]:h-9 [&>svg]:w-9' aria-label='share'>
-          <ShareSVG />
         </button>
       </div>
     </div>
