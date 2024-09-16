eca4fe80a36935c6b82926f92944a7955e1e11d7
origin1508 <origin1508@gmail.com>
2024-09-17 01:43:30 +0900

    ✨ Feat: StoreInfo 스타일 수정, 카테고리 정보 추가 (#43)

--- src/app/posts/[postId]/_components/postContent/StoreInfo.tsx
+++ src/app/posts/[postId]/_components/postContent/StoreInfo.tsx
@@ -4,38 +4,23 @@ import { RatingStarSVG } from '@/components/svg/RatingstarSVG';
 interface StoreInfoProps {
   name: string;
   starRatings: number;
-  menuTag: string[];
+  category: string;
 }
 
-const StoreInfo = ({ name, starRatings, menuTag }: StoreInfoProps) => {
+const StoreInfo = ({ name, starRatings, category }: StoreInfoProps) => {
   return (
-    <div>
-      <div className='flex h-20 items-center'>
-        <div className='grid w-full grid-cols-2 divide-x divide-gray-100'>
-          <div className='flex items-center justify-center gap-2'>
-            <div className='text-gray-700 subTitle-20'>{name}</div>
-            <div>
-              <Point text={name} size='small' />
-            </div>
-          </div>
-          <div className='flex flex-col items-center justify-center gap-2'>
-            <div className='flex items-center gap-1 text-primary-400 subTitle-20'>
-              <span>
-                <RatingStarSVG />
-              </span>
-              <span>{starRatings}</span>
-            </div>
-          </div>
+    <div className='flex flex-col gap-2'>
+      <span className='text-gray-600 body-16'>추천 맛집</span>
+      <div className='flex gap-4'>
+        <div className='flex items-center gap-2'>
+          <h2 className='text-gray-600 subTitle-24'>{name}</h2>
+          <div className='text-gray-300 subTitle-24'>{`(${category})`}</div>
         </div>
-      </div>
-      <div>
-        <div className='mb-2'>
-          <span className='text-gray-600 body-16'>추천 메뉴</span>
-        </div>
-        <div className='flex h-24 flex-wrap items-start gap-2'>
-          {menuTag.map((menu) => (
-            <Chip key={menu} text={menu} />
-          ))}
+        <div className='flex items-center gap-1 text-primary-400 subTitle-20'>
+          <span className='[&>svg]:w-[18px]'>
+            <RatingStarSVG />
+          </span>
+          <span>{starRatings}</span>
         </div>
       </div>
     </div>
