ae93771afa8a96b0c4c40a1932a7e60a121115ac
origin1508 <origin1508@gmail.com>
2024-09-18 22:07:08 +0900

    ✨ Feat: 게시글 dropdown 메뉴 추가 (#43)

--- src/app/posts/[postId]/_components/postContent/UserProfile.tsx
+++ src/app/posts/[postId]/_components/postContent/UserProfile.tsx
@@ -1,6 +1,10 @@
+'use client';
+import { useDropdownHandler } from '@/hooks/useDropdownHandler';
 import { ProfileImage } from '@/components/atoms';
+import { DropdownMenu } from '@/components/molecules';
 import { MoreSVG } from '@/components/svg/MoreSVG';
 import timeDifference from '@/utils/timeDifference';
+import { getClientUserInfo } from '@/utils/getClientUserInfo';
 
 interface UserProfileProps {
   nickname: string;
@@ -13,6 +17,26 @@ const UserProfile = ({
   profileImage,
   createdAt,
 }: UserProfileProps) => {
+  const userInfo = getClientUserInfo();
+  const { isDropdownOpen, dropdownHanlder, dropdownRef } = useDropdownHandler();
+  const dropdownItems = [
+    {
+      label: '피드 보기',
+      onClick: () => {},
+    },
+    ...(userInfo.nickname === nickname
+      ? [
+          {
+            label: '수정하기',
+            onClick: () => {},
+          },
+          {
+            label: '삭제하기',
+            onClick: () => {},
+          },
+        ]
+      : [{ label: '게시물 신고하기', onClick: () => {} }]),
+  ];
   return (
     <div className='flex justify-between'>
       <div className='flex items-center gap-3'>
@@ -22,10 +46,15 @@ const UserProfile = ({
           {timeDifference(createdAt)}
         </span>
       </div>
-      <div className='flex items-center justify-center gap-4'>
-        <button className='[&>svg]:h-9 [&>svg]:w-9' aria-label='more'>
+      <div className='relative' ref={dropdownRef}>
+        <button
+          className='h-full [&>svg]:h-9 [&>svg]:w-9'
+          aria-label='more'
+          onClick={dropdownHanlder}
+        >
           <MoreSVG />
         </button>
+        {isDropdownOpen && <DropdownMenu dropdownItems={dropdownItems} />}
       </div>
     </div>
   );
