063be8658e8e5c96b6dfdb485e2c34c79dd922e7
jms <minsuk815@naver.com>
2024-09-18 23:56:10 +0900

    ✨ Feat: feed, active data fetch (#34)

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,5 +1,6 @@
 import { KOTLIN_SERVER } from '@/constants/baseUrl';
 import { UserData } from '@/types/authType';
+import { getUserToken } from '@/utils/getServerUserInfo';
 type Result<T, E> =
   | { isSuccess: true; data: T }
   | { isSuccess: false; error: E };
@@ -21,5 +22,54 @@ export const getUserProfile = async (
     return { isSuccess: true, data };
   } catch (error) {
     return { isSuccess: false, error: '유저정보조회 에러' };
+  }
+};
+export const getMyfeeds = async () => {
+  const token = await getUserToken();
+  try {
+    const response = await fetch(`${KOTLIN_SERVER}/users/feeds`, {
+      method: 'GET',
+      headers: {
+        accept: '*/*',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getMyActives = async () => {
+  const token = await getUserToken();
+  try {
+    const response = await fetch(`${KOTLIN_SERVER}/users/actives`, {
+      method: 'GET',
+      headers: {
+        accept: '*/*',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getUserFeeds = async (id: string) => {
+  try {
+    const response = await fetch(`${KOTLIN_SERVER}/users/feeds/${id}`, {
+      method: 'GET',
+      headers: {
+        accept: '*/*',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
   }
 };
--- src/app/(auth)/profile/[userId]/page.tsx
+++ src/app/(auth)/profile/[userId]/page.tsx
@@ -1,9 +1,7 @@
 import { ProfilePage } from './_components/ProfilePage';
 export default async function page({ params }: { params: { userId: string } }) {
   const userId = params.userId;
-  //id와 로그인 유저 비교
-  //같으면 myprofile => userInfo로 데이터 가져오기(전화번호가 있음)
-  //다르면 api로 데이터 가져오기
+
   return (
     <>
       <ProfilePage userId={userId} />
