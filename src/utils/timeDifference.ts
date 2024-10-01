0122479d68fca665f91466f5957abac245b4e480
origin1508 <origin1508@gmail.com>
2024-10-01 21:51:10 +0900

    🐛 Fix: 작성일이 제대로 계산되지 않은 에러 수정 (#43)

--- src/utils/timeDifference.ts
+++ src/utils/timeDifference.ts
@@ -20,9 +20,10 @@ const timeDifference = (date: string) => {
     return Math.round(elapsed / msPerHour) + '시간 전';
   else if (elapsed < msPerWeek) return Math.round(elapsed / msPerDay) + '일 전';
   else if (elapsed < msPerMonth)
-    return Math.round(elapsed / msPerMonth) + '개월 전';
+    return Math.round(elapsed / msPerWeek) + '주 전';
   else if (elapsed < msPerYear)
-    return Math.round(elapsed / msPerYear) + '년 전';
+    return Math.round(elapsed / msPerMonth) + '개월 전';
+  else return Math.round(elapsed / msPerYear) + '년 전';
 };
 
 export default timeDifference;
