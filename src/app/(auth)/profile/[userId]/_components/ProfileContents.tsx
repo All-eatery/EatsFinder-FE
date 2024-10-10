14093b1d7416fee3cb9d78dbcd77587c78df0b4e
jms <minsuk815@naver.com>
2024-10-10 21:21:15 +0900

    ✨ Feat: follow(#34)

--- src/app/(auth)/profile/[userId]/_components/ProfileContents.tsx
+++ src/app/(auth)/profile/[userId]/_components/ProfileContents.tsx
@@ -7,6 +7,7 @@ import { Timeline } from './Timeline';
 import { ProfilePageProps } from '@/types/authType';
 import { TimeLineFilter } from './TimeLineFilter';
 import { useTimelineFilterState } from '@/app/(auth)/_hooks/useTimelineFilterState';
+import { FollowList } from './FollowList';
 
 export const ProfileContents = ({
   isOwnProfile,
@@ -28,37 +29,41 @@ export const ProfileContents = ({
   };
 
   return (
-    <div className='flex w-[1368px] flex-col gap-6'>
-      <div className='flex w-full items-center justify-between'>
-        <div className='flex'>
-          {isOwnProfile ? (
-            tabLabels.map((label, i) => {
-              return (
-                <>
-                  <Tab
-                    key={i}
-                    active={activeIndex === i}
-                    onClick={() => handleTabClick(i)}
-                  >
-                    {label}
-                  </Tab>
-                </>
-              );
-            })
-          ) : (
-            <div className='border-b-4 border-gray-800 text-gray-800 title-28'>
-              게시글
-            </div>
+    <>
+      <FollowList title='내 팔로워' />
+
+      <div className='flex w-[1368px] flex-col gap-6'>
+        <div className='flex w-full items-center justify-between'>
+          <div className='flex'>
+            {isOwnProfile ? (
+              tabLabels.map((label, i) => {
+                return (
+                  <>
+                    <Tab
+                      key={i}
+                      active={activeIndex === i}
+                      onClick={() => handleTabClick(i)}
+                    >
+                      {label}
+                    </Tab>
+                  </>
+                );
+              })
+            ) : (
+              <div className='border-b-4 border-gray-800 text-gray-800 title-28'>
+                게시글
+              </div>
+            )}
+          </div>
+
+          {activeIndex === 1 && isOwnProfile && (
+            <>
+              <TimeLineFilter handler={handleFileterState} />
+            </>
           )}
         </div>
-
-        {activeIndex === 1 && isOwnProfile && (
-          <>
-            <TimeLineFilter handler={handleFileterState} />
-          </>
-        )}
+        {contents()}
       </div>
-      {contents()}
-    </div>
+    </>
   );
 };
