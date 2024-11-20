4f3ab92e7196836f043e315753489e661ee839f9
jms <minsuk815@naver.com>
2024-11-20 21:13:29 +0900

    ✨ Feat: 모달 조작 함수 (#69)

--- src/components/molecules/bookmarkButton/BookmarkButton.tsx
+++ src/components/molecules/bookmarkButton/BookmarkButton.tsx
@@ -1,41 +1,48 @@
 'use client';
+import { useBookmarkModal } from '@/app/(auth)/_hooks/useModal';
 import { Button, Checkbox, TextField } from '@/components/atoms';
 import { Modal } from '@/components/organisms';
 import { AddSVG } from '@/components/svg/AddSVG';
 import React, { useState } from 'react';
 
 const BookmarkModalCard = () => {
+  const [isSelected, setIsSelected] = useState(false);
+  const handleCard = () => {
+    setIsSelected(!isSelected);
+  };
   return (
-    <div
-      className='flex items-center justify-start gap-3 rounded-3xl px-5 py-6'
+    <button
+      onClick={handleCard}
+      className={`flex items-center justify-start gap-3 rounded-3xl border-2 px-5 py-6 ${isSelected ? 'border-primary-400' : 'border-transparent'}`}
       style={{
         boxShadow:
           '0 4px 10px rgba(0, 0, 0, 0.05), 0 -4px 10px rgba(45, 31, 31, 0.05), -4px 0 10px rgba(0, 0, 0, 0.05), 4px 0 10px rgba(0, 0, 0, 0.05)',
       }}
     >
-      <Checkbox variant='Checkbox_Ver2' />
+      <Checkbox variant='Checkbox_Ver2' checked={isSelected} />
       <div className='flex flex-col'>
         <p className='text-gray-800 title-24'>기본 리스트</p>
         <p className='text-gray-400 body-18'>4개의 게시물</p>
       </div>
-    </div>
+    </button>
   );
 };
 
 export const BookmarkButton = () => {
   const [Active, setIsActive] = useState(true);
-  const close = () => {};
   const color = Active ? '#0D0D0D' : '#D9D9D9';
+  const { closeModal, confirmButton, isModalOpen, openModal } =
+    useBookmarkModal();
   return (
     <>
-      <Checkbox variant='bookmark' />
+      <Checkbox variant='bookmark' onClick={openModal} />
       <Modal
-        isOpen={true}
+        isOpen={isModalOpen}
         mainButton='확인'
         title='리스트에 추가하기'
         description='리스트를 만들고, 나만의 맛집지도를 완성해 보세요'
-        onClose={close}
-        onMainClick={close}
+        onClose={closeModal}
+        onMainClick={confirmButton}
         size={'medium'}
       >
         <div className='flex flex-col items-center gap-10 px-10 pb-10'>
@@ -57,6 +64,8 @@ export const BookmarkButton = () => {
             </Button>
           </div>
           <div className='flex max-h-[400px] w-full flex-col gap-5 overflow-y-auto px-2 py-2 scrollbar-hide'>
+            <BookmarkModalCard />
+            <BookmarkModalCard />
             <BookmarkModalCard />
             <BookmarkModalCard />
             <BookmarkModalCard />
