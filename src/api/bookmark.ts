2227f19d7ea4740386b69917d3c63881de93d902
jms <minsuk815@naver.com>
2024-11-29 22:22:06 +0900

    ✨ Feat: 북마크 관련 api 함수(#69)

---
+++ src/api/bookmark.ts
@@ -0,0 +1,99 @@
+import { NEST_SERVER } from '@/constants/baseUrl';
+import { getUserToken } from '@/utils/getServerUserInfo';
+
+export const createNewBookmarkList = async (listname: string) => {
+  const token = await getUserToken();
+  const response = await fetch(`${NEST_SERVER}/bookmarks/lists`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      listname,
+    }),
+  });
+};
+export const getBookmarkList = async (cursor: number) => {
+  const token = await getUserToken();
+  const response = await fetch(
+    `${NEST_SERVER}/bookmarks/lists?cursor=${cursor}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+};
+export const renameBookmarkList = async (id: number, title: string) => {
+  const token = await getUserToken();
+  const response = await fetch(`${NEST_SERVER}/bookmarks/lists/id=${id}`, {
+    method: 'PATCH',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      title,
+    }),
+  });
+};
+export const deleteBookmarkList = async (id: number) => {
+  const token = await getUserToken();
+  const response = await fetch(`${NEST_SERVER}/bookmarks/lists/id=${id}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+export const addBookmarkPlaces = async (place: number, lists: number[]) => {
+  const token = await getUserToken();
+  const response = await fetch(`${NEST_SERVER}/bookmarks/places`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      place,
+      lists,
+    }),
+  });
+};
+export const getBookmarkPlaces = async (id: number, cursor: number) => {
+  const token = await getUserToken();
+  const response = await fetch(
+    `${NEST_SERVER}/bookmarks/places/${id}?cursor=${cursor}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+};
+export const moveBookmarkPlaces = async (places: number[], lists: number[]) => {
+  const token = await getUserToken();
+  const response = await fetch(`${NEST_SERVER}/bookmarks/places`, {
+    method: 'PATCH',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      places,
+      lists,
+    }),
+  });
+};
+export const deleteBookmarPlaces = async (placeId: string, listId: number) => {
+  const token = await getUserToken();
+  //placeId => '1,2,3,4' 쉼표로 구분되는 숫자 or all
+  const response = await fetch(
+    `${NEST_SERVER}/bookmarks/places?placeId=${placeId}&listId=${listId}`,
+    {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+};
