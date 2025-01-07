760d06be7cb7f7263c9d73024523cee3e7425041
jms <minsuk815@naver.com>
2025-01-07 23:55:27 +0900

    🐛 Fix: url 쿼리 변경으로 data fetch error 수정 (#63)

--- src/app/(auth)/myeats/_components/bookmark/BookmarkedPlacesList.tsx
+++ src/app/(auth)/myeats/_components/bookmark/BookmarkedPlacesList.tsx
@@ -45,8 +45,7 @@ export const BookmarkedPlacesList = () => {
     openModal: openCreateNewListModal,
     handleNewListName,
   } = createNewListModal();
-  const handleCardClick = (id: string) => {
-    //url을 통해 data를 가져오는게 아닌데 유효할까?
+  const handleCardClick = (id: number) => {
     if (select) return;
     const currentParams = new URLSearchParams(window.location.search);
 
@@ -78,7 +77,7 @@ export const BookmarkedPlacesList = () => {
                   key={item.id}
                   ref={isLastItem ? lastElementRef : null}
                   onClick={() => {
-                    handleCardClick(item.title);
+                    handleCardClick(item.id);
                     checkHandler(item.id);
                   }}
                 >
