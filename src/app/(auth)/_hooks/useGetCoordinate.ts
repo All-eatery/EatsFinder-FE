aa0027b3b960f1004c1627b3aceb52384e98bb87
jms <minsuk815@naver.com>
2025-03-14 22:56:46 +0900

    ✨ Feat: 위치정보 제공 미동의시 기본 주소 설정(#64)

--- src/app/(auth)/_hooks/useGetCoordinate.ts
+++ src/app/(auth)/_hooks/useGetCoordinate.ts
@@ -1,4 +1,8 @@
 import { useEffect, useState } from 'react';
+const DEFAULT_COORDINATE = {
+  lat: 38.19155114124001,
+  lng: 128.601247028514,
+};
 //홈에서부터 가져오고 이를 전역에 저장해두자
 export const useGetCoordinate = () => {
   const [coordinate, setCoordinate] = useState<{
@@ -8,14 +12,19 @@ export const useGetCoordinate = () => {
 
   useEffect(() => {
     if (!coordinate) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setCoordinate({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-      });
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCoordinate({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        () => {
+          setCoordinate(DEFAULT_COORDINATE);
+        },
+      );
     }
-  }, []);
+  }, [coordinate]);
 
   return { coordinate };
 };
