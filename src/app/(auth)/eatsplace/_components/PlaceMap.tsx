271b4f5dc80c8709d48d4245536a4a594ad84b04
jms <minsuk815@naver.com>
2025-03-21 15:12:58 +0900

    ✨ Feat: markerData에 따른 마커 렌더링(#64)

--- src/app/(auth)/eatsplace/_components/PlaceMap.tsx
+++ src/app/(auth)/eatsplace/_components/PlaceMap.tsx
@@ -7,6 +7,8 @@ import {
 import Loading from '@/components/atoms/loading/Loading';
 import { useGetCoordinate } from '../../_hooks/useGetCoordinate';
 import { useEffect, useRef, useState } from 'react';
+import { MarklocationNomalSVG } from '@/components/svg/MarklocationSVG';
+import { AlarmBellSVG } from '@/components/svg/AlarmBellSVG';
 
 export const PlaceMap = () => {
   const mapRef = useRef<kakao.maps.Map | null>(null);
@@ -41,7 +43,47 @@ export const PlaceMap = () => {
   console.log('바운더리', boundary);
 
   //  좌표에 맞는 마커 띄우기
-  // 드래그,휠시 map 재조정
+  //   const svgString = `
+  //   <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+  //     <path d="M35.2763 16.9268C35.2763 23.3466 23.4196 42.5 23.4196 42.5C23.4196 42.5 12.0278 23.3466 12.0278 16.9268C11.5623 10.1847 16.9997 6 23.4196 6C29.8394 6 35.7406 9.95223 35.2763 16.9268Z" fill="#FB5607"/>
+  //     <circle cx="23.6243" cy="16.6944" r="5.57962" fill="white"/>
+  //   </svg>
+  // `;
+
+  // const encodedSvg = encodeURIComponent(svgString);
+  // const dataUrl = `data:image/svg+xml;charset=utf-8,${encodedSvg}`;
+  type Markers = {
+    a: string;
+    lat: number;
+    lng: number;
+    seleted: boolean;
+  };
+  const marekers: Markers[] = [
+    {
+      a: 'aaa',
+      lat: 38.19155,
+      lng: 128.60124,
+      seleted: true,
+    },
+    {
+      a: 'bbb',
+      lat: 38.19165,
+      lng: 128.60134,
+      seleted: true,
+    },
+    {
+      a: 'ccc',
+      lat: 38.19258,
+      lng: 128.60227,
+      seleted: false,
+    },
+    {
+      a: 'ddd',
+      lat: 38.19035,
+      lng: 128.60014,
+      seleted: false,
+    },
+  ];
   return (
     <>
       <h2 className='text-gray-700 subTitle-28'>주변의 맛집 ({address})</h2>
@@ -58,14 +100,33 @@ export const PlaceMap = () => {
           onZoomChanged={getBounday}
           onDrag={getBounday}
         >
-          {/**마커 */}
-          <MapMarker
-            position={{ lat: 37.1527, lng: 127.088 }}
+          {/* *마커
+           * 클릭시 칠드런?
+           * 마커데이터관리는 좀 더 생각해보기
+           * 아니면 차릴 퍼블릭으로
+           */}
+          {/* <MapMarker
+            position={{ lat: 37.563, lng: 126.984 }}
             image={{
               src: '/marker.png',
               size: { width: 48, height: 48 },
             }}
-          />
+          /> */}
+          {marekers.map((marker) => {
+            const url = marker.seleted
+              ? '/marker.png'
+              : '/marker_unSeleted.png';
+            return (
+              <MapMarker
+                position={{ lat: marker.lat, lng: marker.lng }}
+                image={{
+                  src: url,
+                  size: { width: 48, height: 48 },
+                }}
+              />
+            );
+          })}
+
           <MapTypeControl position={'TOPRIGHT'} />
           <ZoomControl position={'RIGHT'} />
         </Map>
