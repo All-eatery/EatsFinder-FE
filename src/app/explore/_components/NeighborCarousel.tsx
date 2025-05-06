52f88cb5b5780193872ac06a1962cecd4593bbce
jms <minsuk815@naver.com>
2025-05-06 15:03:39 +0900

    ✨ Feat: 최근 게시물 조회 (#75)

--- src/app/explore/_components/NeighborCarousel.tsx
+++ src/app/explore/_components/NeighborCarousel.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { CardCarousel } from './CardCarousel';
+import { getNewNeighborPosts } from '@/api/explore';
 const data = new Array(23).fill(0);
 
-export const NeighborCarousel = () => {
+export const NeighborCarousel = async () => {
+  const aaa = await getNewNeighborPosts();
+  console.log('adfsdfdsfdsfdsfsd', aaa);
   return <CardCarousel data={data} title='이웃님의 새로운 게시물' />;
 };
--- src/types/postType.ts
+++ src/types/postType.ts
@@ -17,3 +17,22 @@ export interface PostContentType {
   places: PlaceType;
   starRatings: number;
 }
+
+export interface PostPaginationType {
+  pagination: {
+    totalItems: number;
+    itemsPerPage: number;
+  };
+  items: [
+    {
+      id: number;
+      thumbnailUrl: string;
+      users: {
+        nickname: string;
+        profileImage: string;
+      };
+      likeStatus: boolean;
+    },
+  ];
+  lastItemId: 321;
+}
