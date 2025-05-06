70efd90b46026cb1373e60e98f5f2c00cbbb1728
jms <minsuk815@naver.com>
2025-05-06 11:39:04 +0900

    ✨ Feat: 탐색피드 api 추가 (#75)

---
+++ src/api/explore.ts
@@ -0,0 +1,33 @@
+import { KOTLIN_SERVER, NEST_SERVER } from '@/constants/baseUrl';
+import { getUserToken } from '@/utils/getServerUserInfo';
+
+export const getNewNeighborPosts = async () => {
+  const token = await getUserToken();
+  const response = await fetch(
+    `${KOTLIN_SERVER}/posts/follows?page=0&size=1&sort=string`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  const data = await response.json();
+  return data;
+};
+
+export const getPopularPosts = async () => {
+  const response = await fetch(`${KOTLIN_SERVER}/posts/popular`, {
+    method: 'GET',
+  });
+  const data = await response.json();
+  return data;
+};
+
+export const getAllPosts = async (cursor: number) => {
+  const response = await fetch(`${NEST_SERVER}/posts?cursor=${cursor}`, {
+    method: 'GET',
+  });
+  const data = await response.json();
+  return data;
+};
